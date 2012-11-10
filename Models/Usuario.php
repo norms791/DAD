79913fe8a4129209672e8e30fd72228865341eec
Norma Escobedo <norma791@hotmail.com>
2012-11-10 16:05:44 -0600

    se agrego el modelo usuario

---
+++ Models/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+include_once("connection.php");
+class Usuario{
+	//  Variables globales
+	private $Email;
+	private $Nombre;
+	private $Apellido;
+	
+	function __construct($Email, $Nombre, $Apellido){
+		this->$Email = $Email;
+		this->$Nombre = $Nombre;
+		this->$Apellido = $Apellido;
+	}
+	
