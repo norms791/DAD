a27eb2fedc24cae4eb1c3a6592346430e66a73b9
Norma Escobedo <norma791@hotmail.com>
2012-11-10 23:36:24 -0600

    se agrego el campo contrasena a la tabla usuarios y se modifico latitud y longitud para que sean una sola variable string

--- DataBase/BDProyecto.sql
+++ DataBase/BDProyecto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-11-2012 a las 02:19:40
+-- Tiempo de generación: 11-11-2012 a las 06:35:22
 -- Versión del servidor: 5.5.27
 -- Versión de PHP: 5.4.7
 
@@ -31,8 +31,7 @@ CREATE TABLE IF NOT EXISTS `muebles` (
   `desAbreviada` varchar(60) NOT NULL,
   `desDetallada` varchar(255) NOT NULL,
   `ubicacion` varchar(70) NOT NULL,
-  `latitud` double NOT NULL,
-  `longitud` double NOT NULL,
+  `latitud` varchar(100) NOT NULL,
   `reservado` int(1) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   PRIMARY KEY (`idMueble`),
@@ -43,13 +42,13 @@ CREATE TABLE IF NOT EXISTS `muebles` (
 -- Volcado de datos para la tabla `muebles`
 --
 
-INSERT INTO `muebles` (`idMueble`, `desAbreviada`, `desDetallada`, `ubicacion`, `latitud`, `longitud`, `reservado`, `usuario`) VALUES
-(1, 'Sin Informacion', 'Sin Informacion', 'Sin Informacion', 0, 0, 0, 'norma791@hotmail.com'),
-(2, 'mesa', 'mesa madera', 'sin informacion', 0, 0, 0, 'gaby_roes@hotmail.com'),
-(5, 'nuevo Mueble', 'mueble nuevo de prueba', 'mi casa', 0, 0, 0, 'norma791@hotmail.com'),
-(6, 'nuevo Mueble', 'mueble nuevo de prueba', 'mi casa', 0, 0, 0, 'norma791@hotmail.com'),
-(7, 'nuevo Mueble', 'mueble nuevo de prueba', 'mi casa', 0, 0, 0, 'norma791@hotmail.com'),
-(8, 'nuevo Mueble', 'mueble nuevo de prueba', 'mi casa', 0, 0, 0, 'norma791@hotmail.com');
+INSERT INTO `muebles` (`idMueble`, `desAbreviada`, `desDetallada`, `ubicacion`, `latitud`, `reservado`, `usuario`) VALUES
+(1, 'ola', 'Sin Informacion', 'Sin Informacion', '0', 0, 'norma791@hotmail.com'),
+(2, 'mesa', 'mesa madera', 'sin informacion', '0', 0, 'gaby_roes@hotmail.com'),
+(5, 'mesa chica', 'mueble nuevo de prueba', 'mi casa', '0', 0, 'norma791@hotmail.com'),
+(6, 'nuevo Mueble', 'mueble nuevo de prueba ola', 'mi casa', '0', 0, 'norma791@hotmail.com'),
+(7, 'piano', 'mueble nuevo de prueba', 'mi casa', '0', 0, 'norma791@hotmail.com'),
+(8, 'silla', 'mueble ola nuevo de prueba', 'mi casa', '0', 0, 'norma791@hotmail.com');
 
 -- --------------------------------------------------------
 
@@ -82,6 +81,7 @@ INSERT INTO `reservacion` (`idReservacion`, `idMueble`, `idUsuario`) VALUES
 
 CREATE TABLE IF NOT EXISTS `usuario` (
   `Email` varchar(50) NOT NULL,
+  `contrasena` varchar(12) NOT NULL,
   `Nombre` varchar(50) NOT NULL,
   `Apellido` varchar(50) NOT NULL,
   PRIMARY KEY (`Email`),
@@ -92,11 +92,12 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`Email`, `Nombre`, `Apellido`) VALUES
-('gaby_roes@hotmail.com', 'Gabriela', 'Rodriguez'),
-('mary@florreo.mx', 'Rosario', 'Robles'),
-('norma791@hotmail.com', 'Norma', 'Escobedo'),
-('rosa.roble@florreo.mx', 'Rosario', 'Robles');
+INSERT INTO `usuario` (`Email`, `contrasena`, `Nombre`, `Apellido`) VALUES
+('cacmartinez@gmail.com', '', 'Carlos', 'Compean'),
+('gaby_roes@hotmail.com', '', 'Gabriela', 'Rodriguez'),
+('mary@florreo.mx', '', 'Rosario', 'Robles'),
+('norma791@hotmail.com', '', 'Norma', 'Escobedo'),
+('rosa.roble@florreo.mx', '', 'Rosario', 'Robles');
 
 --
 -- Restricciones para tablas volcadas
