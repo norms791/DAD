248b66714d00d1cf0f46e1f3bfdf7971fafb6608
unknown <cronolink@gmail.com>
2012-11-19 00:28:25 -0600

    modificaciones

--- Views/buscarArt.php
+++ Views/buscarArt.php
@@ -18,26 +18,18 @@
 					<th>Ubicación</th>
 				</tr>";
 	
-	if($num!=0){
-		for($i=0;$i<1;$i++){
-			echo "			<tr>\n";
-			echo "				<td><img src='".$foto[$i]."' width='400' /></td>\n";
-			echo "				<td>".$muebles[$i]->getDesAbreviada()."</td>\n";
-			echo "				<td>".$muebles[$i]->getUbicacion()."</td>\n";
-			echo "			</tr>\n";
-		}
-	} else {
+	for($i=0;$i<1;$i++){
 		echo "			<tr>\n";
-		echo "				<td><img src='".$nofoto."' width='400' /></td>\n";
-		echo "				<td>".$noexiste[0]->getDesAbreviada()."</td>\n";
-		echo "				<td>".$noexiste[0]->getUbicacion()."</td>\n";
+		echo "				<td><img src='".$foto[$i]."' width='400' /></td>\n";
+		echo "				<td>".$muebles[$i]->getDesAbreviada()."</td>\n";
+		echo "				<td>".$muebles[$i]->getUbicacion()."</td>\n";
 		echo "			</tr>\n";
 	}
 
 	echo "		</table>\n";
 	
 	} else if($vacio==true){
-		echo "		<h1>No se recibió información para poder buscar artículos.</h1>";
+		echo "		<h1>No hubo resultados en la busqueda.</h1>";
 	}
 	mysql_close($conexion);
 ?>
