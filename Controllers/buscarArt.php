ac5c8bb0bf74628e55964900cdd3a9a48117076a
David Guzman Araiza <dajavax@gmail.com>
2012-11-18 22:06:23 -0600

    Se arregla error en variables necesariar para header.

--- Controllers/buscarArt.php
+++ Controllers/buscarArt.php
@@ -1,17 +1,18 @@
 <?php
 	/* toma el valor de la barra de busqueda de la pagina de inicio */
-	//$keyword = $_GET["keyword"];
-	$keyword = "mesa";
-	include_once("../Models/Muebles.php"); //Se incluye la conexion a la base de datos
+	$keyword = $_GET["parametro"];
 	session_start();
+	include_once("../Models/Muebles.php"); //Se incluye la conexion a la base de datos
+
 	if(isset($_SESSION['mail'])){
+		include_once("../Models/Usuario.php");
+		$usuario= Usuario::obtenerUsuario($_SESSION['mail']);
 		//aqui va el header para usuarios con cuenta
 		include("../Views/header.php");
 	} else {
 		//aqui va el header para usuarios sin cuenta
 		include("../Views/headerPrincipal.php");
 	}
-	
 	$muebles = Muebles::buscaMuebles($keyword);
 	if($muebles){
 		include("../Views/buscarArt.php");
